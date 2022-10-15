3f9fd189d5bcb527835c6ccf9dfb5ee18b53faa4
IvanovArtemNA <IvanovArtemNA@gmail.com>
2022-10-16 00:47:07 +0300

    correct

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,3 +1,21 @@
+// (() => {
+//     const menuBtnRef = document.querySelector('[data-menu-btn]');
+//     const mobileMenuRef = document.querySelector('[data-mobile-menu]');
+//     const navLinks = document.querySelectorAll('.nav__link');
+//     menuBtnRef.addEventListener('click', closeMenu);
+
+//     navLinks.forEach(navLink => navLink.addEventListener('click', closeMenu));
+
+//     function closeMenu() {
+//         const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+//         menuBtnRef.classList.toggle('is-open');
+//         menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+//         mobileMenuRef.classList.toggle('is-open');
+//     }
+// })();
+
 (() => {
     const menuBtnRef = document.querySelector('[data-menu-btn]');
     const mobileMenuRef = document.querySelector('[data-mobile-menu]');
@@ -9,7 +27,7 @@
     function closeMenu() {
         const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-        menuBtnRef.classList.toggle('is-open');
+        menuBtnRef.classList.toggle('is-active');
         menuBtnRef.setAttribute('aria-expanded', !expanded);
 
         mobileMenuRef.classList.toggle('is-open');
